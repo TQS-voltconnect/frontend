c8448ece7e19880344f961a47b12a02bb7c1e1c6
carolmprata <carolinaprata@ua.pt>
2025-05-14 16:25:36 +0100

    frontend started main page and stations searcher

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import StationSearch from './components/StationSearch';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        React + Tailwind Test
-      </h1>
-      <button
-        onClick={() => setCount(count + 1)}
-        className="px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded shadow transition duration-300"
-      >
-        Clicked {count} {count === 1 ? "time" : "times"}
-      </button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<StationSearch />} />
+      </Routes>
+    </Router>
   );
 }
 
